71b8049af8e07159489acac7e1e9399fe0949264
Nicholas <phobereromain@gmail.com>
2024-04-21 21:17:54 +0100

    added search icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,18 +3,21 @@ import React from 'react'
 function Header() {
     return (
         <div className='h-20 0 shadow-lg flex justify-between items-center px-5 text-xl font-mono'>
-            <div>Recipes</div>
-            {/* <div className='text-2xl m-2  px-4 flex items-center font-bold hover:scale-105 transition duration-500'>
-            PengFood.com
-        </div> */}
+            <div className='flex space-x-4'>
+
+                <button className='text-black'>&#128269;</button>
+                <button>Recipes</button>
+            </div>
             <a href='/'>
                 <img
                     src='https://www.pickuplimes.com/static/images/logo/pul_logo_main_flat_987_247.9cf5ed7b1374.png'
                     className='h-16 animate-pulse'
                 />
             </a>
-
-            <div>About</div>
+            <div>
+                <div></div>
+                <button>About</button>
+            </div>
 
         </div>
     )
