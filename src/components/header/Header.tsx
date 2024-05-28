a90c58a068c3009e2385bb9b13a0c2cd5d93f5b2
Nicholas <phobereromain@gmail.com>
2024-05-28 20:36:35 +0100

    added ingredients section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -24,17 +24,13 @@ const Header: React.FC<HeaderProps> = ({showMenu, setShowMenu}) => {
                 <button className='' onClick={handleClick}>
                     <MenuIcon />
                 </button>
-                <button>Recipes</button>
             </div>
             <a href='/'>
                 <div className='text-3xl font-bold animate-pulse text-amber-900' style={{ fontFamily: 'Brush Script MT, Brush Script Std, cursive' }}>
                     NikosCookBook&#129369;
                 </div>
             </a>
-            <div>
                 <div></div>
-                <a href='/about'>About</a>
-            </div>
 
         </div>
     )
--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -1,53 +1,79 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import PeopleIcon from '@mui/icons-material/People';
 import AssessmentIcon from '@mui/icons-material/Assessment';
+
+
 function Recipe() {
+  const data = {
+    title: 'Quiche Lorraine',
+    introducion: "Quiche Lorraine is a classic French dish from the Lorraine region,\
+    featuring a rich filling of eggs, cream, and lardons (bacon) in a buttery pastry crust.\
+    Quiche Lorraine is a classic French dish from the Lorraine region, \
+    featuring a rich filling of eggs, cream, and lardons (bacon) in a buttery pastry crust",
+    imageUrl: 'https://cdn.pickuplimes.com/cache/6c/6c/6c6ce42cc549d2cb368e59424388cf36.jpg',
+    prepTime: 45,
+    serves: 4,
+    difficulty: 'Intermediate'
+  }
+
   return (
-    <div className="h-auto xl:w-4/5 p-10 mt-4">
-       {/* Title and Image with recipe intro*/}
-        <div className="h-full flex xl:space-x-5 flex-col xl:flex-row space-y-5 xl:space-y-0">
 
-          <div className="w-full xl:w-1/2 space-y-5  ">
-            <p className="text-6xl font-bold" style={{fontFamily: 'sans'}}>Quiche Lorraine</p>
-            <p className="text-xl italic">Quiche Lorraine is a classic French dish from the Lorraine region, 
-              featuring a rich filling of eggs, cream, and lardons (bacon) in a buttery pastry crust.
-              Quiche Lorraine is a classic French dish from the Lorraine region, 
-              featuring a rich filling of eggs, cream, and lardons (bacon) in a buttery pastry crust.
-              </p>
+    <div className="h-auto xl:w-4/5 p-5 mt-4">
+      {/* Title and Image with recipe intro*/}
+      <div className="h-full flex xl:space-x-5 flex-col xl:flex-row space-y-5 xl:space-y-0">
 
-            <div className="flex text-lg space-x-10 items-end ">
+        <div className="w-full xl:w-1/2 space-y-5 bg-red0 ">
+          <p className="text-6xl font-bold text-amber-900" style={{ fontFamily: 'unset' }}>{data.title}</p>
+          <p className="text-lg italic">
+            {data.introducion}
+          </p>
 
-              <div className=" flex flex-col rounded-xl  justify-center items-center">
-                <AccessAlarmIcon 
-                  fontSize='large'
-                />
-                <p className=''>45 min</p>
-              </div>
-              <div className=" flex flex-col rounded-xl  justify-center items-center">
-                <PeopleIcon 
-                  fontSize='large'
-                />
-                <p className=''>4 People</p>
-              </div>
-              <div className=" flex flex-col rounded-xl justify-center items-center">
-                <AssessmentIcon 
-                  fontSize='large'
-                />
-                <p className=''>Medium</p>
-              </div>
+          <div className="flex w-full justify-center text-md space-x-1 xl:space-x-10 items-end text-amber-50">
+
+            <div className=" shadow-lg  bg-amber-300 p-3  w-1/3 flex flex-col rounded-xl  justify-center items-center">
+              <AccessAlarmIcon
+                fontSize='large'
+              />
+              <p className=''>{data.prepTime} min</p>
             </div>
-
-
+            <div className=" shadow-lg bg-amber-300 p-3 w-1/3 flex flex-col rounded-xl  justify-center items-center">
+              <PeopleIcon
+                fontSize='large'
+              />
+              <p className=''>{data.serves} People</p>
+            </div>
+            <div className=" shadow-lg bg-amber-300 p-3 w-1/3 flex flex-col rounded-xl justify-center items-center">
+              <AssessmentIcon
+                fontSize='large'
+              />
+              <p className=''>{data.difficulty}</p>
+            </div>
           </div>
 
-          <div className="w-full xl:w-1/2 xl:p-5">
-            <img 
-              src="https://cdn.pickuplimes.com/cache/6c/6c/6c6ce42cc549d2cb368e59424388cf36.jpg"
-              className="rounded-xl"
-            />
-          </div>
 
         </div>
+
+        <div className="w-full xl:w-1/2 xl:p-5">
+          <img
+            src={data.imageUrl}
+            className="rounded-xl"
+          />
+        </div>
+
+      </div>
+
+      {/* Ingredients */}
+
+      <div>
+        <p className='text-4xl font-semibold'>Ingredients</p>
+        
+      </div>
+
+      {/* Method */}
+
+      <div>
+        <p className='text-4xl font-semibold'>Method</p>
+      </div>
 
     </div>
   )
